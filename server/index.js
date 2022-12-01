0d0b675c26d732481e856d8147f0f9b02d784fb6
Viraj Baswana <virajbaswana@gmail.com>
2022-12-01 17:33:38 +0530

    correction in server & ApolloClient in App.js

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.use(
   })
 );
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'../client/build' )))
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'../', 'client', 'build', 'index.html')))
-}
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname,'../client/build' )))
+//   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'../', 'client', 'build', 'index.html')))
+// }
 
 app.listen(port, console.log(`Server running on port ${port}`));
